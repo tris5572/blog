621128ece3ea6ff913011e6b061fd80dac65de90
tris <nexus4fortune@gmail.com>
2023-02-11 20:42:51 +0900

    画像リンク修正

--- src/scripts/util.ts
+++ src/scripts/util.ts
@@ -1,5 +1,10 @@
 // デプロイ時のサブディレクトリを考慮したリンク文字列を生成する。
 // 使用するためには astro.config.mjs の defineConfig に base を設定すること。
 export function href(s: string): string {
-  return `${import.meta.env.BASE_URL}${s}`;
+  let str = s;
+  // スラッシュから始まるとき、それを削除する。
+  if (str.charAt(0) === '/') {
+    str = str.substring(1);
+  }
+  return `${import.meta.env.BASE_URL}${str}`;
 }
