82ff310aa1e3cdbbcdffd6f0e5cc4aabd2c8d9fc
tris <nexus4fortune@gmail.com>
2023-02-15 16:54:05 +0900

    全体の動作がひとまず完成

--- src/content/config.ts
+++ src/content/config.ts
@@ -16,6 +16,7 @@ const blog = defineCollection({
       .transform((str) => (str ? new Date(str) : undefined)),
     heroImage: z.string().optional(),
     tags: z.array(z.string()),
+    draft: z.boolean().optional(),
   }),
 });
 
